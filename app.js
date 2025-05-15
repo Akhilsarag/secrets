3f080385cf3444dc3a6493929ab4ac4c97d0f7c0
Akhil Saragandla <akhilsaragandla32003@gmail.com>
2025-05-15 20:39:40 +0530

    Made some internal changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const encrypt = require('mongoose-encryption');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -10,19 +11,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
-const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://user3:Akhil1234@cluster0.c9dkcry.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 const trySchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    secrets: [{ content: String }]
 });
 
 const secretKey = 'Thisislittlesecret.';
-const jwtSecret = 'jwtSuperSecretKey'; // In real apps, store this in .env
+const jwtSecret = 'jwtSuperSecretKey';
 
 trySchema.plugin(encrypt, { secret: secretKey, encryptedFields: ['password'] });
-
 const User = mongoose.model('User', trySchema);
 
 function authenticateToken(req, res, next) {
@@ -68,14 +68,13 @@ app.post('/login', async (req, res) => {
     const password = req.body.password;
 
     try {
-        const foundUser = await User.findOne({ email: email });
+        const foundUser = await User.findOne({ email });
         if (!foundUser) return res.send('No user found with this email');
 
         if (foundUser.password === password) {
             const userPayload = { email: foundUser.email };
             const token = jwt.sign(userPayload, jwtSecret, { expiresIn: '1h' });
-
-            res.cookie('token', token, { httpOnly: true, secure: false }); // secure: true in production
+            res.cookie('token', token, { httpOnly: true });
             res.redirect('/secrets');
         } else {
             res.send('Incorrect password');
@@ -86,12 +85,67 @@ app.post('/login', async (req, res) => {
     }
 });
 
-app.get('/secrets', authenticateToken, (req, res) => {
-    const dummySecrets = [
-        { content: 'I love pizza' },
-        { content: 'I sing in the shower' }
-    ];
-    res.render('secrets', { secrets: dummySecrets });
+app.get('/secrets', authenticateToken, async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.user.email });
+        res.render('secrets', { secrets: user.secrets || [] });
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    }
+});
+
+app.get('/submit', authenticateToken, (req, res) => {
+    res.render('submit');
+});
+
+app.post('/submit', authenticateToken, async (req, res) => {
+    const submittedSecret = req.body.secret;
+    const userEmail = req.user.email;
+
+    try {
+        await User.findOneAndUpdate(
+            { email: userEmail },
+            { $push: { secrets: { content: submittedSecret } } }
+        );
+        res.redirect('/secrets');
+    } catch (err) {
+        console.log(err);
+        res.redirect('/submit');
+    }
+});
+
+app.post('/edit', authenticateToken, async (req, res) => {
+    const secretId = req.body.id;
+    const newContent = req.body.newContent;
+
+    try {
+        const user = await User.findOne({ email: req.user.email });
+        const secret = user.secrets.id(secretId);
+        if (secret) {
+            secret.content = newContent;
+            await user.save();
+        }
+        res.redirect('/secrets');
+    } catch (err) {
+        console.log(err);
+        res.redirect('/secrets');
+    }
+});
+
+app.post('/delete', authenticateToken, async (req, res) => {
+    const secretId = req.body.id;
+
+    try {
+        await User.updateOne(
+            { email: req.user.email },
+            { $pull: { secrets: { _id: secretId } } }
+        );
+        res.redirect('/secrets');
+    } catch (err) {
+        console.log(err);
+        res.redirect('/secrets');
+    }
 });
 
 app.get('/logout', (req, res) => {
@@ -100,5 +154,5 @@ app.get('/logout', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('server started');
+    console.log('Server started on port 3000');
 });
